be734a88f5ec90bbd2de9a5a46ff0fa9051d186d
Ivan Prignano <ivanprignano@gmail.com>
2025-11-05 13:00:06 +0100

    remove header

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,6 @@ import { BoardContextProvider } from './components/BoardContextProvider';
 export default function App() {
   return (
     <div className="min-h-screen text-gray-900">
-      <header className="border-b bg-white">
-        <div className="mx-auto max-w-6xl px-4 py-4 flex items-center justify-between">
-          <h1 className="text-xl font-semibold">todo-loco</h1>
-        </div>
-      </header>
       <main className="mx-auto max-w-6xl px-4 py-10">
         <BoardContextProvider>
           <BoardArea />
