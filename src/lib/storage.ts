f33b5f21957dec162468cd77d003188c933115ae
Ivan Prignano <ivanprignano@gmail.com>
2025-11-05 22:35:31 +0100

    replace indexedDB storage with localstorage

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,30 +1,11 @@
-import { openDB, type IDBPDatabase } from 'idb';
 import { debounce } from 'es-toolkit';
 import type { BoardState } from '../types';
 
-const DB_NAME = 'todo-loco-db';
-const DB_VERSION = 1;
-const STATE_STORE = 'state';
-const ROOT_KEY = 'root';
-
-let dbPromise: Promise<IDBPDatabase> | null = null;
-
-function getDb(): Promise<IDBPDatabase> {
-  if (!dbPromise) {
-    dbPromise = openDB(DB_NAME, DB_VERSION, {
-      upgrade(database) {
-        if (!database.objectStoreNames.contains(STATE_STORE)) {
-          database.createObjectStore(STATE_STORE);
-        }
-      },
-    });
-  }
-  return dbPromise;
-}
-
+const STATE_STORE_KEY = 'tl_state';
 export async function loadState<T = BoardState>(): Promise<T | undefined> {
-  const db = await getDb();
-  return (await db.get(STATE_STORE, ROOT_KEY)) as T | undefined;
+  return localStorage.getItem(STATE_STORE_KEY)
+    ? JSON.parse(localStorage.getItem(STATE_STORE_KEY)!)
+    : undefined;
 }
 
 export function saveStateDebounced<T = BoardState>(state: T): void {
@@ -34,6 +15,5 @@ export function saveStateDebounced<T = BoardState>(state: T): void {
 }
 
 export async function saveState<T = BoardState>(state: T): Promise<void> {
-  const db = await getDb();
-  await db.put(STATE_STORE, state, ROOT_KEY);
+  localStorage.setItem(STATE_STORE_KEY, JSON.stringify(state));
 }
