6f8f76d2b12b9856edf2e798fb99101d61c223d8
Ivan Prignano <ivanprignano@gmail.com>
2025-11-05 19:10:43 +0100

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BoardArea } from './components/BoardArea';
 import { BoardContextProvider } from './components/BoardContextProvider';
 
+import './styles/app.css';
+
 export default function App() {
   return (
     <div className="min-h-screen text-gray-900">
@@ -9,6 +11,14 @@ export default function App() {
           <BoardArea />
         </BoardContextProvider>
       </main>
+      <footer className="mt-10 text-center">
+        <a
+          className="logo-link"
+          target="_blank"
+          href="https://github.com/iprignano/todo-loco"
+          rel="noreferrer noopener"
+        />
+      </footer>
     </div>
   );
 }
